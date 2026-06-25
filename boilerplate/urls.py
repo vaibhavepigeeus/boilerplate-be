fc23a0fefa55f37028a9bfcca13f3bdb7274aa31
vaibhavepigeeus <vaibhav@epigeeus.com>
2026-06-25 17:33:21 +0530

    Setup BE boiler plate

---
+++ boilerplate/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path('api/documents/', include('documents.urls')),
+    path('api/filemanagement/', include('filemanagement.urls')),
+    path('api/workflow/', include('workflow.urls')),
+    path('api/audit/', include('audit.urls')),
+]
